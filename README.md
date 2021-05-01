6e3950c8e2013cffccd7b3c3e931a41142b3658d
Евгений Руденко <evgeniirudenko1978@gmail.com>
2021-05-01 12:00:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WordPress.com for Desktop
+
+The WordPress Desktop project is now part of the [Calypso monorepo](https://github.com/Automattic/wp-calypso) and this repository has been archived. All future releases will be published to Automattic/wp-calypso.
---
+++ test/tests/lib/pages/reader-page.js
@@ -0,0 +1,12 @@
+/** @format */
+
+const { By } = require( 'selenium-webdriver' );
+const AsyncBaseContainer = require( '../async-base-container' );
+
+class ReaderPage extends AsyncBaseContainer {
+	constructor( driver ) {
+		super( driver, By.css( '.is-section-reader' ) );
+	}
+}
+
+module.exports = ReaderPage;
